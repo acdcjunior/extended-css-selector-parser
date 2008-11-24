3a504ed201978ddfba1b8c533cd3b5eb3ac1bd71
waldbaer <waldbaer@5edc6d46-b6c6-43e4-afc4-535a15193f73>
2008-11-24 14:22:24 +0000

    initial check-in

---
+++ cssparser/src/test/java/com/steadystate/css/parser/Important2Test.java
@@ -0,0 +1,88 @@
+package com.steadystate.css.parser;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+import org.w3c.css.sac.CSSException;
+import org.w3c.css.sac.CSSParseException;
+import org.w3c.css.sac.ErrorHandler;
+import org.w3c.css.sac.InputSource;
+import org.w3c.css.sac.Parser;
+
+import junit.framework.TestCase;
+
+public class Important2Test extends TestCase implements ErrorHandler
+{
+
+	private Parser sacParser;
+
+	public void testCSS1()
+    {
+        this.testCSS(new SACParserCSS1());
+    }
+
+    public void testCSS2()
+    {
+        this.testCSS(new SACParserCSS2());
+    }
+
+    public void testCSS21()
+    {
+        this.testCSS(new SACParserCSS21());
+    }
+
+
+    private void testCSS(Parser sacParser)
+    {
+    	this.sacParser = sacParser;
+    	sacParser.setErrorHandler(this);
+        InputStream is = getClass().getClassLoader().getResourceAsStream("important2.css");
+        assertNotNull(is);
+        Reader r = new InputStreamReader(is);
+        InputSource source = new InputSource(r);
+        try
+        {
+            sacParser.parseStyleSheet(source);
+		}
+        catch (CSSException e)
+        {
+            assertFalse(e.getLocalizedMessage(), true);
+		}
+        catch (IOException e)
+        {
+            assertFalse(e.getLocalizedMessage(), true);
+		}
+    }
+
+	public void error(CSSParseException e) throws CSSException
+	{
+		if ("http://www.w3.org/TR/CSS21/".equals(this.sacParser.getParserVersion()))
+		{
+			assertFalse(e.getLocalizedMessage(), true);
+		}
+		else
+		{
+			assertTrue(true);
+		}
+	}
+
+	public void fatalError(CSSParseException e) throws CSSException
+	{
+        assertFalse(e.getLocalizedMessage(), true);
+	}
+
+	public void warning(CSSParseException e) throws CSSException
+	{
+		if ("http://www.w3.org/TR/REC-CSS2/".equals(this.sacParser.getParserVersion()))
+		{
+			assertTrue(true);
+		}
+		else
+		{
+			assertFalse(e.getLocalizedMessage(), true);
+		}
+	}
+
+}
