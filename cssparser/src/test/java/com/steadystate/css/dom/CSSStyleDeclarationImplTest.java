997d96ab63a1702eef026df4c1118a6f08bc49b0
waldbaer <waldbaer@5edc6d46-b6c6-43e4-afc4-535a15193f73>
2009-09-11 11:54:20 +0000

    added another test method

--- cssparser/src/test/java/com/steadystate/css/dom/CSSStyleDeclarationImplTest.java
+++ cssparser/src/test/java/com/steadystate/css/dom/CSSStyleDeclarationImplTest.java
@@ -1,7 +1,11 @@
 package com.steadystate.css.dom;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Reader;
 import java.io.StringReader;
 
@@ -15,6 +19,7 @@ import com.steadystate.css.parser.CSSOMParser;
  * Unit tests for {@link CSSStyleDeclarationImpl}.
  * 
  * @author Daniel Gredler
+ * @author waldbaer
  */
 public class CSSStyleDeclarationImplTest extends TestCase {
 
@@ -57,6 +62,28 @@ public class CSSStyleDeclarationImplTest extends TestCase {
         CSSStyleDeclarationImpl style = (CSSStyleDeclarationImpl) parser.parseStyleDeclaration(source);
 
         assertEquals("", style.getCssText());
+    }
+
+    public void testSerialize() throws Exception
+    {
+        InputStream is = getClass().getClassLoader().getResourceAsStream("basic.css");
+        assertNotNull(is);
+
+        CSSOMParser parser = new CSSOMParser();
+
+        Reader r = new InputStreamReader(is);
+        InputSource source = new InputSource(r);
+        CSSStyleDeclarationImpl style = (CSSStyleDeclarationImpl) parser.parseStyleDeclaration(source);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(baos);
+        oos.writeObject(style);
+        oos.flush();
+        oos.close();
+        ObjectInputStream ois = new ObjectInputStream(
+            new ByteArrayInputStream(baos.toByteArray()));
+        Object o = ois.readObject();
+        ois.close();
+        assertEquals(style, o);
     }
 
 }
