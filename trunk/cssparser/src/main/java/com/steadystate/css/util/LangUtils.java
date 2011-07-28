b69842252a339d504adcf381d21c77426bfaa117
rbri <rbri@5edc6d46-b6c6-43e4-afc4-535a15193f73>
2011-07-28 19:26:57 +0000

    unified formating, copyright and cleanup

--- trunk/cssparser/src/main/java/com/steadystate/css/util/LangUtils.java
+++ trunk/cssparser/src/main/java/com/steadystate/css/util/LangUtils.java
@@ -1,6 +1,4 @@
 /*
- * $Id: LangUtils.java,v 1.1 2008-08-14 08:17:55 waldbaer Exp $
- *
  * CSS Parser Project
  *
  * Copyright (C) 1999-2005 David Schweinsberg.  All rights reserved.
@@ -23,38 +21,31 @@
  *
  * http://cssparser.sourceforge.net/
  * mailto:davidsch@users.sourceforge.net
+ *
  */
+
 package com.steadystate.css.util;
 
-public class LangUtils
-{
-
+public final class LangUtils {
     public static final int HASH_SEED = 17;
     public static final int HASH_OFFSET = 37;
 
-    private LangUtils()
-    {
+    private LangUtils() {
     }
 
-
-    public static int hashCode(final int seed, final int hashcode)
-    {
+    public static int hashCode(final int seed, final int hashcode) {
         return seed * HASH_OFFSET + hashcode;
     }
 
-    public static int hashCode(final int seed, final boolean b)
-    {
+    public static int hashCode(final int seed, final boolean b) {
         return hashCode(seed, b ? 1 : 0);
     }
 
-    public static int hashCode(final int seed, final Object obj)
-    {
+    public static int hashCode(final int seed, final Object obj) {
         return hashCode(seed, obj != null ? obj.hashCode() : 0);
     }
-    
-    public static boolean equals(final Object obj1, final Object obj2)
-    {
+
+    public static boolean equals(final Object obj1, final Object obj2) {
         return obj1 == null ? obj2 == null : obj1.equals(obj2);
     }
-
 }
