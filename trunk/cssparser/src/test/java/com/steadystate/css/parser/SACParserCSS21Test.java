e6448777b2b11dd34079ebb25310a4f219ebe49c
rbri <rbri@5edc6d46-b6c6-43e4-afc4-535a15193f73>
2011-08-01 18:56:17 +0000

    CSS 2.1 parser now ignores any '@import' rule that occurs inside a block.

--- trunk/cssparser/src/test/java/com/steadystate/css/parser/SACParserCSS21Test.java
+++ trunk/cssparser/src/test/java/com/steadystate/css/parser/SACParserCSS21Test.java
@@ -1,9 +1,7 @@
 /*
- * $Id: SACParserCSS21Test.java,v 1.4 2009-09-11 11:54:38 waldbaer Exp $
- *
  * CSS Parser Project
  *
- * Copyright (C) 1999-2008 David Schweinsberg.  All rights reserved.
+ * Copyright (C) 1999-2011 David Schweinsberg.  All rights reserved.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -23,7 +21,9 @@
  *
  * http://cssparser.sourceforge.net/
  * mailto:davidsch@users.sourceforge.net
+ *
  */
+
 package com.steadystate.css.parser;
 
 import java.io.InputStream;
@@ -35,26 +35,24 @@ import junit.framework.Assert;
 
 import org.junit.Test;
 import org.w3c.css.sac.AttributeCondition;
-import org.w3c.css.sac.CSSException;
-import org.w3c.css.sac.CSSParseException;
 import org.w3c.css.sac.CombinatorCondition;
 import org.w3c.css.sac.Condition;
 import org.w3c.css.sac.ConditionalSelector;
 import org.w3c.css.sac.DescendantSelector;
-import org.w3c.css.sac.ErrorHandler;
 import org.w3c.css.sac.InputSource;
-import org.w3c.css.sac.Parser;
 import org.w3c.css.sac.Selector;
 import org.w3c.css.sac.SelectorList;
 import org.w3c.css.sac.SimpleSelector;
+import org.w3c.dom.css.CSSRule;
+import org.w3c.dom.css.CSSRuleList;
 import org.w3c.dom.css.CSSStyleSheet;
+
+import com.steadystate.css.ErrorHandler;
 
 /**
  * @author rbri
  */
 public class SACParserCSS21Test {
-
-    private static final Parser SAC_PARSER = new SACParserCSS21();
 
     @Test
     public void selectorList() throws Exception {
@@ -88,27 +86,39 @@ public class SACParserCSS21Test {
         selectorType("a:visited", Selector.SAC_CONDITIONAL_SELECTOR);
         selectorType("a:active", Selector.SAC_CONDITIONAL_SELECTOR);
 
-        selectorType("h1:first-line", Selector.SAC_DESCENDANT_SELECTOR, Selector.SAC_ELEMENT_NODE_SELECTOR, Selector.SAC_PSEUDO_ELEMENT_SELECTOR);
-        selectorType("a:first-letter", Selector.SAC_DESCENDANT_SELECTOR, Selector.SAC_ELEMENT_NODE_SELECTOR, Selector.SAC_PSEUDO_ELEMENT_SELECTOR);
+        selectorType("h1:first-line", Selector.SAC_DESCENDANT_SELECTOR, Selector.SAC_ELEMENT_NODE_SELECTOR,
+                Selector.SAC_PSEUDO_ELEMENT_SELECTOR);
+        selectorType("a:first-letter", Selector.SAC_DESCENDANT_SELECTOR, Selector.SAC_ELEMENT_NODE_SELECTOR,
+                Selector.SAC_PSEUDO_ELEMENT_SELECTOR);
 
-        selectorType("h1 a", Selector.SAC_DESCENDANT_SELECTOR, Selector.SAC_ELEMENT_NODE_SELECTOR, Selector.SAC_ELEMENT_NODE_SELECTOR);
-        selectorType("h1  a", Selector.SAC_DESCENDANT_SELECTOR, Selector.SAC_ELEMENT_NODE_SELECTOR, Selector.SAC_ELEMENT_NODE_SELECTOR);
+        selectorType("h1 a", Selector.SAC_DESCENDANT_SELECTOR, Selector.SAC_ELEMENT_NODE_SELECTOR,
+                Selector.SAC_ELEMENT_NODE_SELECTOR);
+        selectorType("h1  a", Selector.SAC_DESCENDANT_SELECTOR, Selector.SAC_ELEMENT_NODE_SELECTOR,
+                Selector.SAC_ELEMENT_NODE_SELECTOR);
     }
 
     @Test
     public void condition() throws Exception {
-        conditionType("a#id.class:link", Condition.SAC_AND_CONDITION, Condition.SAC_AND_CONDITION, Condition.SAC_ID_CONDITION, Condition.SAC_CLASS_CONDITION, Condition.SAC_PSEUDO_CLASS_CONDITION);
-        conditionType("a#id.class", Condition.SAC_AND_CONDITION, Condition.SAC_ID_CONDITION, Condition.SAC_CLASS_CONDITION);
-        conditionType("a#id:link", Condition.SAC_AND_CONDITION, Condition.SAC_ID_CONDITION, Condition.SAC_PSEUDO_CLASS_CONDITION);
+        conditionType("a#id.class:link", Condition.SAC_AND_CONDITION, Condition.SAC_AND_CONDITION,
+                Condition.SAC_ID_CONDITION, Condition.SAC_CLASS_CONDITION, Condition.SAC_PSEUDO_CLASS_CONDITION);
+        conditionType("a#id.class", Condition.SAC_AND_CONDITION, Condition.SAC_ID_CONDITION,
+                Condition.SAC_CLASS_CONDITION);
+        conditionType("a#id:link", Condition.SAC_AND_CONDITION, Condition.SAC_ID_CONDITION,
+                Condition.SAC_PSEUDO_CLASS_CONDITION);
         conditionType("a#id", Condition.SAC_ID_CONDITION);
-        conditionType("a.class:link", Condition.SAC_AND_CONDITION, Condition.SAC_CLASS_CONDITION, Condition.SAC_PSEUDO_CLASS_CONDITION);
+        conditionType("a.class:link", Condition.SAC_AND_CONDITION, Condition.SAC_CLASS_CONDITION,
+                Condition.SAC_PSEUDO_CLASS_CONDITION);
         conditionType("a.class", Condition.SAC_CLASS_CONDITION);
         conditionType("a:link", Condition.SAC_PSEUDO_CLASS_CONDITION);
-        conditionType("#id.class:link", Condition.SAC_AND_CONDITION, Condition.SAC_AND_CONDITION, Condition.SAC_ID_CONDITION, Condition.SAC_CLASS_CONDITION, Condition.SAC_PSEUDO_CLASS_CONDITION);
-        conditionType("#id.class", Condition.SAC_AND_CONDITION, Condition.SAC_ID_CONDITION, Condition.SAC_CLASS_CONDITION);
-        conditionType("#id:link", Condition.SAC_AND_CONDITION, Condition.SAC_ID_CONDITION, Condition.SAC_PSEUDO_CLASS_CONDITION);
+        conditionType("#id.class:link", Condition.SAC_AND_CONDITION, Condition.SAC_AND_CONDITION,
+                Condition.SAC_ID_CONDITION, Condition.SAC_CLASS_CONDITION, Condition.SAC_PSEUDO_CLASS_CONDITION);
+        conditionType("#id.class", Condition.SAC_AND_CONDITION, Condition.SAC_ID_CONDITION,
+                Condition.SAC_CLASS_CONDITION);
+        conditionType("#id:link", Condition.SAC_AND_CONDITION, Condition.SAC_ID_CONDITION,
+                Condition.SAC_PSEUDO_CLASS_CONDITION);
         conditionType("#id", Condition.SAC_ID_CONDITION);
-        conditionType(".class:link", Condition.SAC_AND_CONDITION, Condition.SAC_CLASS_CONDITION, Condition.SAC_PSEUDO_CLASS_CONDITION);
+        conditionType(".class:link", Condition.SAC_AND_CONDITION, Condition.SAC_CLASS_CONDITION,
+                Condition.SAC_PSEUDO_CLASS_CONDITION);
         conditionType(".class", Condition.SAC_CLASS_CONDITION);
         conditionType(":link", Condition.SAC_PSEUDO_CLASS_CONDITION);
     }
@@ -127,112 +137,202 @@ public class SACParserCSS21Test {
         attributeConditionValue(":active", "active");
     }
 
-
-    private void selectorList(String cssText, int length) throws Exception {
-        SelectorList selectors = createSelectors(cssText);
+    private void selectorList(final String cssText, final int length) throws Exception {
+        final SelectorList selectors = createSelectors(cssText);
         Assert.assertEquals(length, selectors.getLength());
     }
 
-    private void selectorType(String cssText, int... selectorTypes) throws Exception {
-        SelectorList selectors = createSelectors(cssText);
-        Selector selector = selectors.item(0);
+    private void selectorType(final String cssText, final int... selectorTypes) throws Exception {
+        final SelectorList selectors = createSelectors(cssText);
+        final Selector selector = selectors.item(0);
         Assert.assertEquals(selectorTypes[0], selector.getSelectorType());
         if (selectorTypes[0] == Selector.SAC_DESCENDANT_SELECTOR) {
-            DescendantSelector descendantSelector = (DescendantSelector) selector;
-            Selector ancestor = descendantSelector.getAncestorSelector();
+            final DescendantSelector descendantSelector = (DescendantSelector) selector;
+            final Selector ancestor = descendantSelector.getAncestorSelector();
             Assert.assertEquals(selectorTypes[1], ancestor.getSelectorType());
-            SimpleSelector simple = descendantSelector.getSimpleSelector();
+            final SimpleSelector simple = descendantSelector.getSimpleSelector();
             Assert.assertEquals(selectorTypes[2], simple.getSelectorType());
         }
     }
 
-    private void conditionType(String cssText, int... conditionTypes) throws Exception {
-        Condition condition = createCondition(cssText);
+    private void conditionType(final String cssText, final int... conditionTypes) throws Exception {
+        final Condition condition = createCondition(cssText);
         conditionType(condition, 0, conditionTypes);
     }
 
-    private int conditionType(Condition condition, int initial, int... conditionTypes) {
+    private int conditionType(final Condition condition, int initial, final int... conditionTypes) {
         Assert.assertEquals(conditionTypes[initial], condition.getConditionType());
         if (conditionTypes[initial] == Condition.SAC_AND_CONDITION) {
-            CombinatorCondition combinatorCondition = (CombinatorCondition) condition;
-            Condition first = combinatorCondition.getFirstCondition();
-            Condition second = combinatorCondition.getSecondCondition();
+            final CombinatorCondition combinatorCondition = (CombinatorCondition) condition;
+            final Condition first = combinatorCondition.getFirstCondition();
+            final Condition second = combinatorCondition.getSecondCondition();
             initial = conditionType(first, ++initial, conditionTypes);
             initial = conditionType(second, ++initial, conditionTypes);
         }
         return initial;
     }
 
-    private void attributeConditionValue(String cssText, String value) throws Exception {
-        Condition condition = createCondition(cssText);
-        AttributeCondition attributeCondition = (AttributeCondition) condition;
+    private void attributeConditionValue(final String cssText, final String value) throws Exception {
+        final Condition condition = createCondition(cssText);
+        final AttributeCondition attributeCondition = (AttributeCondition) condition;
         Assert.assertEquals(value, attributeCondition.getValue());
     }
 
-    private SelectorList createSelectors(String cssText) throws Exception {
-        InputSource source = new InputSource(new StringReader(cssText));
-        return SAC_PARSER.parseSelectors(source);
+    private SelectorList createSelectors(final String cssText) throws Exception {
+        final InputSource source = new InputSource(new StringReader(cssText));
+        return new SACParserCSS21().parseSelectors(source);
     }
 
-    private Condition createCondition(String cssText) throws Exception {
-        SelectorList selectors = createSelectors(cssText);
-        Selector selector = selectors.item(0);
-        ConditionalSelector conditionalSelector = (ConditionalSelector) selector;
+    private Condition createCondition(final String cssText) throws Exception {
+        final SelectorList selectors = createSelectors(cssText);
+        final Selector selector = selectors.item(0);
+        final ConditionalSelector conditionalSelector = (ConditionalSelector) selector;
         return conditionalSelector.getCondition();
     }
 
     @Test
     public void dojoCSS() throws Exception {
-        InputStream is = getClass().getClassLoader().getResourceAsStream("dojo.css");
+        final InputStream is = getClass().getClassLoader().getResourceAsStream("dojo.css");
         Assert.assertNotNull(is);
 
-        Reader r = new InputStreamReader(is);
-        InputSource source = new InputSource(r);
+        final Reader r = new InputStreamReader(is);
+        final InputSource source = new InputSource(r);
 
-        SACParserCSS21 sp = new SACParserCSS21();
-        CSSOMParser parser = new CSSOMParser(sp);
-        CSSStyleSheet sheet = parser.parseStyleSheet(source, null, null);
+        final CSSOMParser parser = new CSSOMParser(new SACParserCSS21());
+        final ErrorHandler errorHandler = new ErrorHandler();
+        parser.setErrorHandler(errorHandler);
+
+        final CSSStyleSheet sheet = parser.parseStyleSheet(source, null, null);
 
         Assert.assertEquals(17, sheet.getCssRules().getLength());
-    }
-
-    @Test
-    public void css() throws Exception {
-        InputStream is = getClass().getClassLoader().getResourceAsStream("test.css");
-        Assert.assertNotNull(is);
-
-        Reader r = new InputStreamReader(is);
-        InputSource source = new InputSource(r);
-
-        CSSOMParser parser = new CSSOMParser(new SACParserCSS21());
-        CSSStyleSheet sheet = parser.parseStyleSheet(source, null, null);
-
-        Assert.assertEquals(78, sheet.getCssRules().getLength());
+        Assert.assertEquals(0, errorHandler.getErrorCount());
+        Assert.assertEquals(0, errorHandler.getFatalErrorCount());
+        Assert.assertEquals(0, errorHandler.getWarningCount());
     }
 
     @Test
     public void emptyCSS() throws Exception {
-        Reader r = new StringReader("");
-        InputSource source = new InputSource(r);
+        final InputSource source = new InputSource(new StringReader(""));
+        final CSSOMParser parser = new CSSOMParser(new SACParserCSS21());
 
-        CSSOMParser parser = new CSSOMParser(new SACParserCSS21());
-        final StringBuilder errors = new StringBuilder();
-        final ErrorHandler handler = new ErrorHandler() {
-            public void warning(final CSSParseException exception) throws CSSException {
-                errors.append(exception.getMessage()).append(" ");
-            }
-            public void fatalError(final CSSParseException exception) throws CSSException {
-                errors.append(exception.getMessage()).append(" ");
-            }
-            public void error(final CSSParseException exception) throws CSSException {
-                errors.append(exception.getMessage()).append(" ");
-            }
-        };
-        parser.setErrorHandler(handler);
-        
-        CSSStyleSheet sheet = parser.parseStyleSheet(source, null, null);
+        final ErrorHandler errorHandler = new ErrorHandler();
+        parser.setErrorHandler(errorHandler);
+
+        final CSSStyleSheet sheet = parser.parseStyleSheet(source, null, null);
 
         Assert.assertEquals(0, sheet.getCssRules().getLength());
-        Assert.assertEquals("", errors.toString().trim());
+        Assert.assertEquals(0, errorHandler.getErrorCount());
+        Assert.assertEquals(0, errorHandler.getFatalErrorCount());
+        Assert.assertEquals(0, errorHandler.getWarningCount());
+    }
+
+    /**
+     * @see http://www.w3.org/TR/CSS2/syndata.html#at-rules
+     */
+    @Test
+    public void atRules1() throws Exception {
+        final String css = "@import 'subs.css';\n"
+            + "h1 { color: blue }\n"
+            + "@import 'list.css';\n";
+
+        final InputSource source = new InputSource(new StringReader(css));
+        final CSSOMParser parser = new CSSOMParser(new SACParserCSS21());
+
+        final ErrorHandler errorHandler = new ErrorHandler();
+        parser.setErrorHandler(errorHandler);
+
+        final CSSStyleSheet sheet = parser.parseStyleSheet(source, null, null);
+
+        Assert.assertEquals(1, errorHandler.getErrorCount());
+        Assert.assertEquals(0, errorHandler.getFatalErrorCount());
+        Assert.assertEquals(0, errorHandler.getWarningCount());
+
+        final CSSRuleList rules = sheet.getCssRules();
+
+        Assert.assertEquals(2, rules.getLength());
+
+        CSSRule rule = rules.item(0);
+        Assert.assertEquals("@import url(subs.css);", rule.getCssText());
+
+        rule = rules.item(1);
+        Assert.assertEquals("h1 { color: blue }", rule.getCssText());
+    }
+
+    /**
+     * @see http://www.w3.org/TR/CSS2/syndata.html#at-rules
+     */
+    @Test
+    public void atRules2() throws Exception {
+        final String css = "@import 'subs.css';\n"
+            + "@media print {\n"
+            + "  @import 'print-main.css';\n"
+            + "  body { font-size: 10pt }\n"
+            + "}\n"
+            + "h1 {color: blue }\n";
+
+        final InputSource source = new InputSource(new StringReader(css));
+        final CSSOMParser parser = new CSSOMParser(new SACParserCSS21());
+
+        final ErrorHandler errorHandler = new ErrorHandler();
+        parser.setErrorHandler(errorHandler);
+
+        final CSSStyleSheet sheet = parser.parseStyleSheet(source, null, null);
+
+        Assert.assertEquals(1, errorHandler.getErrorCount());
+        Assert.assertEquals(0, errorHandler.getFatalErrorCount());
+        Assert.assertEquals(0, errorHandler.getWarningCount());
+
+        final CSSRuleList rules = sheet.getCssRules();
+
+        Assert.assertEquals(3, rules.getLength());
+
+        CSSRule rule = rules.item(0);
+        Assert.assertEquals("@import url(subs.css);", rule.getCssText());
+
+        rule = rules.item(1);
+        Assert.assertEquals("@media print {body { font-size: 10pt } }", rule.getCssText());
+
+        rule = rules.item(2);
+        Assert.assertEquals("h1 { color: blue }", rule.getCssText());
+    }
+
+    /**
+     * @import after rule
+     * @throws Exception
+     */
+    @Test
+    public void atRules2b() throws Exception {
+        final String css = "@import 'subs.css';\n"
+            + "@media print {\n"
+            + "  body { font-size: 10pt }\n"
+            + "  @import 'print-main.css';\n"
+            + "}\n"
+            + "h1 {color: blue }\n";
+
+        final InputSource source = new InputSource(new StringReader(css));
+        final CSSOMParser parser = new CSSOMParser(new SACParserCSS21());
+
+        final ErrorHandler errorHandler = new ErrorHandler();
+        parser.setErrorHandler(errorHandler);
+
+        final CSSStyleSheet sheet = parser.parseStyleSheet(source, null, null);
+
+        Assert.assertEquals(1, errorHandler.getErrorCount());
+        Assert.assertEquals(0, errorHandler.getFatalErrorCount());
+        Assert.assertEquals(0, errorHandler.getWarningCount());
+
+        final CSSRuleList rules = sheet.getCssRules();
+
+        Assert.assertEquals(3, rules.getLength());
+
+        CSSRule rule = rules.item(0);
+        Assert.assertEquals("@import url(subs.css);", rule.getCssText());
+
+        rule = rules.item(1);
+        Assert.assertEquals("@media print {body { font-size: 10pt } }", rule.getCssText());
+
+        rule = rules.item(2);
+        Assert.assertEquals("h1 { color: blue }", rule.getCssText());
     }
 }
+
